0c39003a469e75849baa69c77f95be1f1d0412fd
Yuxi <1291481253@qq.com>
2020-02-18 17:02:18 -0500

    post.js schema

--- models/post.js
+++ models/post.js
@@ -6,7 +6,22 @@ const mongoose = require('mongoose')
 // Making a Mongoose model a little differently: a Mongoose Schema
 // Allows us to add additional functionality.
 const PostSchema = new mongoose.Schema({
+    category: {
+        type: String,
+        required: true,
+    },
 
+    publisher: String,
+    type: Number,
+    courseId: String,
+    sectionId: String,
+    content: String,
+    publishTime: Date,
+    activateTime: Date,
+    privacy: Boolean,
+    status: Number,
+    attended: [{memberId: String}],
+    requested: [{reqId: String, message: String}]
 })
 
 
