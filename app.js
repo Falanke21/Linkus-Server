a9230939afe0b78ff8ee6d7fdf38dcf922a5d1f0
Yuxi <1291481253@qq.com>
2020-03-08 17:45:32 -0400

    post POST fixed

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var signupRouter = require('./routes/signup');
+var postRouter = require('./routes/post');
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/signup', signupRouter);
+app.use('/post', postRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/post.js
+++ routes/post.js
@@ -5,35 +5,27 @@ const { mongoose } = require('../db/mongoose')
 
 /* POST login. */
 router.post('/', function(req, res, next) {
-	const user = new User({
+	console.log("Enter the router");
+	const post = new Post({
 		category: req.body.category,
 		publisher: req.body.publisher,
 		type: req.body.type,
 		courseId: req.body.courseId,
 		sectionId: req.body.sectionId,
 		content: req.body.content,
+		publishTime: req.body.publishTime,
+		activateTime: req.body.activateTime,
 		privacy: req.body.privacy,
 		status: req.body.status,
 		attended: [],
 		requested: []
 	})
+	console.log("post created");
 
-	User.findByEmailPassword(email, password).then((user) => {
-	    if (!user) {
-			console.log(`Can't find user with email: ${email}`);
-            res.send({
-				authorized: false
-			});
-        } else {
-			// Valid email address and password
-			// More code in /models/user.js
-			console.log(`Authorized user with email: ${email}`);
-            res.send({
-				authorized: true
-			})
-        }
-    }).catch((error) => {
-		res.status(500).send();
+	post.save().then((post) => {
+	    res.send(post)
+    }, (error) => {
+        res.status(400).send(error)
     })
 });
 
