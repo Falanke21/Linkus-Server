a50e71d3967419c9ce67c7b32d90ba10d9ecd2fd
Falanke21 <1065314944@qq.com>
2020-05-01 19:28:56 -0400

    modified post route

--- routes/post.js
+++ routes/post.js
@@ -1,32 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { Post } = require('../models/post')
-const { mongoose } = require('../db/mongoose')
+const { Post } = require("../models/post");
+const { mongoose } = require("../db/mongoose");
 
-/* POST login. */
-router.post('/', function(req, res, next) {
-	console.log("Enter the router");
-	const post = new Post({
-		category: req.body.category,
-		publisher: req.body.publisher,
-		type: req.body.type,
-		courseId: req.body.courseId,
-		sectionId: req.body.sectionId,
-		content: req.body.content,
-		publishTime: req.body.publishTime,
-		activateTime: req.body.activateTime,
-		privacy: req.body.privacy,
-		status: req.body.status,
-		attended: [],
-		requested: []
-	})
-	console.log("post created");
+/* POST a new post. */
+router.post("/", function (req, res, next) {
+    console.log("Enter the router");
+    const post = new Post({
+        category: req.body.category,
+        publisher: req.body.publisher,
+        type: req.body.type,
+        courseId: req.body.courseId,
+        sectionId: req.body.sectionId,
+        content: req.body.content,
+        privacy: req.body.privacy,
+        status: req.body.status,
+        attended: [],
+        requested: [],
+    });
+    console.log("post created");
 
-	post.save().then((post) => {
-	    res.send(post)
-    }, (error) => {
-        res.status(400).send(error)
-    })
+    post.save().then(
+        (post) => {
+            res.send(post);
+        },
+        (error) => {
+            res.status(400).send(error);
+        }
+    );
 });
 
 module.exports = router;
